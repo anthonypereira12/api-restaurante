45585161842895d67e788953ce649b9f57b33c67
Anthony Pereira <anthonyrocha1212@gmail.com>
2025-10-29 23:04:35 -0300

    first commit

--- index.ts
+++ index.ts
@@ -2,12 +2,12 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 
-import auth from "./src/routes/auth.ts";
-import restaurants from "./src/routes/restaurants.ts";
-import items from "./src/routes/items.ts";
-import orders from "./src/routes/orders.ts";
-import reviews from "./src/routes/reviews.ts";
-import analytics from "./src/routes/analytics.ts";
+import auth from "./src/routes/auth.js";
+import restaurants from "./src/routes/restaurants.js";
+import items from "./src/routes/items.js";
+import orders from "./src/routes/orders.js";
+import reviews from "./src/routes/reviews.js";
+import analytics from "./src/routes/analytics.js";
 
 const app = express();
 app.use(cors());
@@ -22,5 +22,5 @@ app.use("/orders", orders);
 app.use("/reviews", reviews);
 app.use("/analytics", analytics);
 
-const port = process.env.PORT || 3333;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log("API on http://localhost:" + port));
