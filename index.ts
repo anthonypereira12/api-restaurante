09932392c39e56666641e8179a13e445f8e97964
Anthony Pereira <anthonyrocha1212@gmail.com>
2025-10-29 21:45:22 -0300

    aabb

--- index.ts
+++ index.ts
@@ -2,12 +2,12 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 
-import auth from "./routes/auth.js";
-import restaurants from "./routes/restaurants.js";
-import items from "./routes/items.js";
-import orders from "./routes/orders.js";
-import reviews from "./routes/reviews.js";
-import analytics from "./routes/analytics.js";
+import auth from "./src/routes/auth.js";
+import restaurants from "./src/routes/restaurants.js";
+import items from "./src/routes/items.js";
+import orders from "./src/routes/orders.js";
+import reviews from "./src/routes/reviews.js";
+import analytics from "./src/routes/analytics.js";
 
 const app = express();
 app.use(cors());
